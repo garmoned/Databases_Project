a227d5b0d26026ad37f0d405a1f7a80217061ab3
Edward <garmonedward@gmail.com>
2021-04-23 16:44:21 -0500

    initial commit

---
+++ generateData.sql
@@ -0,0 +1,30 @@
+INSERT INTO Role VALUES
+(1,”Operator”,17.00),
+(2,”Cleaner”,11.00),
+(3,”Manager”,20.00);
+
+INSERT INTO Plant VALUES
+(1, NULL, “Luminant Forney Station”),
+(2, NULL, “Leeward Renewable Energy”),
+(3, NULL, “Denison Dam Hydroelectric Power Plant”)),
+(4, NULL, “Spencer Generating Station”),
+(5, NULL, “Exelon Power Texas”),
+(6, NULL, “River Bend Station”),
+(7, NULL, “Exelon Power Texas”),
+(8, NULL, “AZZ Wind”),
+(9, NULL, “Panda Power Plant”);
+
+INSERT INTO Hydro_Plants VALUES
+(1, 3.05, 'Seymour River'),
+(2, 4.19, 'Ogallala River'),
+(3, 3.96, 'Hueco River');
+
+INSERT INTO Solar_Plants VALUES
+(4, 2059, 1060),
+(5, 1020, 940),
+(6, 1569, 1024);
+
+INSERT INTO Wind_Plants VALUES
+(7, 60.43, 160),
+(8, 72.43, 185),
+(9, 75.54, 230);
---
+++ generateEmployees.py
@@ -0,0 +1,49 @@
+import random
+
+employee_id = 0 
+
+namesFile = open("./names.txt",mode="r")
+employeeDataFile = open("./employeeData.txt", mode="w")
+
+names = []
+
+for name in namesFile.readlines():
+    names.append(name.strip())
+
+for plant_id in range(1,10):
+
+    
+    managed_by = None
+
+    for employee_offset in range (0,5):
+
+        role = random.randint(1,2)
+
+        #if the employee is a 
+        #the first employees will
+        #be managers
+        if(employee_offset == 0 ):
+            role = 3
+
+        works_at = plant_id
+        #get random name
+        first_name = random.choice(names)
+        last_name = random.choice(names)
+
+
+        sqlValues = f'({employee_id},"{first_name}","{last_name}",{plant_id},{managed_by} ,{role})'
+
+        if(managed_by == None):
+            sqlValues = f'({employee_id},"{first_name}","{last_name}",{plant_id},NULL ,{role})'
+
+
+        employeeDataFile.write(sqlValues + "\n")
+
+        if(employee_offset == 0 ):
+            managed_by = employee_id
+
+        employee_id += 1 
+
+      
+        
+        
