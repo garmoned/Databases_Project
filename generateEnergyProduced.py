834034c5900a75cede1a9419ea37176787a035ba
Don Sip <64543001+donavin-sip@users.noreply.github.com>
2021-05-04 16:51:28 -0500

    Add files via upload
    update file, added temp/test text files used in the energy produced data generation

--- generateEnergyProduced.py
+++ generateEnergyProduced.py
@@ -12,6 +12,8 @@ with open('energyData.txt') as file, open("energySold.txt", mode="w") as out:
             d = date.date()
             #temp = [int(energy) for energy in line.split() if energy.isdigit()]
             #energy_sold = temp.index(3)
-            energy_produced = random.randrange(2000*200, 3000*200) # 2.43 MW per turbine, average 180-230 turbines, similar power to other green sources
+            energy_produced = random.randrange(2000*200, 6000*200)
+            # 2.43 MW per turbine, average 180-230 turbines, similar power to other green sources
+            # 150,000 MWh, 6250000 kW, on average for large hydro plant facilities
             out.write(f"({energy_produced}, {plantID}, {d})\n")
         plantID += 1
