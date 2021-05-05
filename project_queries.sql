0e70d13dc989d84828f9e9f17e7eb657d7d621e9
Edward <garmonedward@gmail.com>
2021-05-05 15:20:16 -0500

    modified queries

--- project_queries.sql
+++ project_queries.sql
@@ -4,7 +4,7 @@ from Plant join Energy_Produced E on Plant.plant_id = E.plant;
 
 -- Gets the total income per day for the entire month
 SELECT Energy_sold.date, ROUND(CAST(kilowatts_sold AS DECIMAL(7, 2)) * CAST(dollar_per_kilowatt AS DECIMAL(7, 2)), 2) AS 'total_income_per_day'
-FROM Energy_sold;
+FROM Energy_sold
 
 -- Gets how much money each plant spent on workers per day
 
